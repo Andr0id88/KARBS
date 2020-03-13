61b28ff4528286fbeec79f063a319e304caff8c8
Andr0id88 <1988.hansen@gmail.com>
2020-03-13 22:18:46 +0100

    Uploaded by script, no commit msg added

--- customInstalls.sh
+++ customInstalls.sh
@@ -2,40 +2,39 @@
 
 # Setup plugged for nvim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Update\install all vim plugins
 nvim -E -c "PlugUpdate|visual|q|q"
 
+bleedingnvim=ppa:neovim-ppa/unstable
+
+if ! grep -q "^deb .*$bleedingnvim" /etc/apt/sources.list/etc/apt/sources.list.d/*; then
+    add-apt-repository ppa:neovim-ppa/unstable -y && apt-get install neovim -y
+fi
+
+nvim -E -c "UpdateRemotePlugins|visual|q|q"
+
 # This is needed to get deoplete working in vim.
 # :UpdateRemotePlugins
 
-#Test out this command in cli:
-nvim -E -c "UpdateRemotePlugins|visual|q|q"
 
-# Installs omf and then the lambda theme
-# curl -L https://get.oh-my.fish | fish
-# omf install lambda
-# bash
 
-curl -L git.io/antigen > .config/zsh/antigen.zsh
+curl -L git.io/antigen > ~/.config/zsh/antigen.zsh
 
 # Create home folder structure
 
-mkdir -p $HOME/devops/{ansible,docker,jenkins,k8s,puppet,vagrant,powershell}
-
-mkdir -p $HOME/Documents/{cheatsheets,dotfiles,pkglist}
-
-mkdir -p $HOME/development/{eclipse,projects,visualstudiocode}
-
-mkdir -p $HOME/github/{external,myrepos}
-
-mkdir -p $HOME/Videos/{tutorials,series,movies}
-
-mkdir $HOME/Music $HOME/sourcecode
-
-mkdir $HOME/Pictures/{wallpaper,people,logo,meme}
+mkdir -p $HOME/devops/{ansible,docker,jenkins,k8s,puppet,vagrant,powershell} \
+$HOME/Documents/{cheatsheets,dotfiles,pkglist} \
+$HOME/development/{eclipse,projects,visualstudiocode} \
+$HOME/github/{external,myrepos} \
+$HOME/Videos/{tutorials,series,movies} \
+$HOME/Music $HOME/sourcecode \
+$HOME/Pictures/{wallpaper,people,logo,meme} 2> /dev/null
 
 # Download my most important git repo
-
-git clone https://github.com/Andr0id88/vimwiki ~/
+if [ -z "($ls -A ~/vimwiki)" ]; then
+   echo "git clone https://github.com/Andr0id88/vimwiki ~/vimwiki"
+else
+   echo "You allready have files in your vimwiki folder"
+fi
